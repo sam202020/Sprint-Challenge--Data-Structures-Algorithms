3261e6acb3cc4249e6a085c2fe7082f51e6e6aa0
Steve <shalomlandsman@gmail.com>
2018-07-20 14:56:10 -0400

    fuck this shit

--- data_structures/ex_1/binary_search_tree.py
+++ data_structures/ex_1/binary_search_tree.py
@@ -5,25 +5,57 @@ class BinarySearchTree:
     self.right = None
 
   def depth_first_for_each(self, cb):
-    root = self
-    main_traversed = []
-    def traverse_left(root, cb):
-      traversed = []
-      current = root
-      while root.left != None:
-        cb(current)
-        traversed.append(current)
-        current = current.left
-      return traversed
-    if root.left:
-      main_traversed = traverse_left(root, cb)
-    for i in reversed(main_traversed):
-      if main_traversed[i].right:
-        current = main_traversed[i].right
-        cb(current)
-        main_traversed.pop(current)
-      else:
-        main_traversed.pop(current)
+    current = self
+    cb(current)
+    traversed = []
+    traversed.append(current)
+    while current.left:
+      print(current.value)
+      current = current.left
+      current.depth_first_for_each(cb)
+    for index, i in reversed(list(enumerate(traversed))):
+      if i.right:
+        current = i.right
+        current.depth_first_for_each(cb)
+
+    # def main_traverse(current, main_traversed, cb):
+    #   if not main_traversed:
+    #     return
+    #   if not current.left:
+    #     for index, i in reversed(list(enumerate(main_traversed))):
+
+    #   if not s
+      # root = self
+      # def traverse_left(root, cb):
+      #   traversed = []
+      #   current = root
+      #   while root.left != None:
+      #     cb(current)
+      #     traversed.append(current)
+      #     current = current.left
+      #   return traversed
+      # if root.left:
+      #   main_traversed = traverse_left(root, cb)
+      # for i in reversed(main_traversed):
+      #   if main_traversed[i].right:
+      #     current = main_traversed[i].right
+      #     cb(current)
+      #     main_traversed.pop(current)
+      #   else:
+      #     main_traversed.pop(current)
+      # def recurse_left(root, cb):
+      #   if not root.left:
+      #     return root
+      #   cb(root.value)
+      #   main_traversed.append(root)
+      #   root = root.left
+      #   recurse_left(root, cb)
+      # root = recurse_left(root, cb)
+      # for index, i in reversed(list(enumerate(main_traversed))):
+      #   if i.right:
+      #     root = i.right
+      #     main_traversed.pop(index)
+      #     recurse_left(root, cb)
     
   def breadth_first_for_each(self, cb):
     pass
@@ -69,3 +101,8 @@ bst.insert(3)
 bst.insert(7)
 bst.insert(9)
 print(bst.value)
+max_val = bst.get_max
+print(max_val)
+# arr = []
+# cb = lambda x: arr.append(x)
+# bst.depth_first_for_each(cb)
